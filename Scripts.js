7c62cdc0ee3c647aa4d19f972bde102cf3b1f360
Ndimande <mkatroso.deep@gmail.com>
2023-04-24 22:23:46 +0200

    IWA13

--- Scripts.js
+++ Scripts.js
@@ -1,28 +1,25 @@
-let state = 'idle'
-let user = null
-let calculated = '1'
+let state = 'idle';
+let user = null;
+let calculated = 1;
 
 // Only allowed to change below
 
+const logCalc = () => {
+  calculated++;
+};
 
-const logCalc = () => { 
-    const isString = typeof calculated ('numerical-string')
-    const calculatedAsNumber = isString ? calculated : parseNumber(calculated)
-    calculated === calculatedAsNumber + 1 
-}
-
-const  calcUser = () => {
-  logCalc
-  if (calculated > 2) user = 'John'
-  if (calculated > 2) state = 'requesting'
-  if (calculated > 3) state = 'idle'
-}
+const calcUser = () => {
+  logCalc();
+  if (calculated > 2) user = 'John';
+  if (calculated > 2) state = 'requesting';
+  if (calculated > 3) state = 'idle';
+};
 
 const checkUser = () => {
-	if (user && state === 'requesting') {
-		console.log(`User: ${user} (${calculated})`)
-	}
-}
+  if (user && state === 'requesting' && calculated === 3) {
+    console.log(`User: ${user} (${calculated})`);
+  }
+};
 
 // Only allowed to change code above
 
